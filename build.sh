cad2b70c9e2402529692e09afb5b460d03725b64
Stefan Baryakov <sbarakov@talend.com>
2016-05-26 09:50:46 +0000

    remove reference

--- build.sh
+++ build.sh
@@ -11,6 +11,3 @@ zip -r lambda_port_allocator.zip .
 popd
 mv package_dir/lambda_port_allocator.zip .
 
-# upload to https://s3.amazonaws.com/us-east-1-devops-talend-com/lambdas/lambda_port_allocator.zip
-aws s3 cp lambda_port_allocator.zip s3://us-east-1-devops-talend-com/lambdas/
-
